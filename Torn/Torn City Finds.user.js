030fe9f3e8c79f873cea437bfa233ece59332595
AquaRegia <emanuel.malmquist@gmail.com>
2021-01-18 22:39:14 +0100

    Update Torn City Finds.user.js

--- Torn/Torn City Finds.user.js
+++ Torn/Torn City Finds.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name             Torn City Finds
 // @namespace        
-// @version          0.4
+// @version          0.5
 // @description
 // @author           AquaRegia
 // @match            https://www.torn.com/city.php*
-// @grant            none
+// @grant            GM_addStyle
 // ==/UserScript==
 
 const API_KEY = "";
@@ -18,6 +18,25 @@ async function sleep(ms)
 function stringifyDate(date)
 {
     return date.toISOString().replace("T", " ").replace("Z", "").split(".")[0];
+}
+
+function resizeTable()
+{
+    for(let i = 1; i < 4; i++)
+    {
+        let max = 0;
+
+        document.querySelectorAll(`#cityFindTable td:nth-child(${i}), #cityFindTable th:nth-child(${i})`).forEach(e =>
+        {
+            e.style.width = "0";
+            max = e.offsetWidth > max ? e.offsetWidth : max;
+        });
+
+        document.querySelectorAll(`#cityFindTable td:nth-child(${i}), #cityFindTable th:nth-child(${i})`).forEach(e =>
+        {
+            e.style.width = max + "px";
+        });
+    }
 }
 
 async function getAverageItemCost(id)
@@ -52,20 +71,33 @@ async function getAverageItemCost(id)
 
 async function calculateTotalItemValue(items, element)
 {
-    var parent = element.parentNode;
-    parent.innerHTML = "Loading...";
+    element.innerHTML = "Loading...";
 
     var sum = 0;
+    var promises = [];
 
     for(let item of items)
     {
-        sum += await getAverageItemCost(item.id) * item.amount;
-        await sleep(2000);
+        promises.push(getAverageItemCost(item.id).then(function(value)
+        {
+            document.querySelector("#cityFind-" + item.id).innerHTML = "$" + (value*item.amount).toLocaleString();
+            document.querySelector("#cityFind-" + item.id).style.textAlign = "right";
+            resizeTable();
+            return value*item.amount;
+        }));
 
-        parent.innerHTML = (((items.indexOf(item)+1) / items.length)*100).toFixed(0) + "%";
+        parent.innerHTML = Math.max(0, (((promises.length-3) / (items.length-3))*100)).toFixed(0) + "%";
+
+        if(promises.length >= 10)
+        {
+            await sleep(2000);
+        }
     }
 
-    parent.innerHTML = "~$" + window.addCommas(sum);
+    element.innerHTML = "$" + ((await Promise.all(promises)).reduce((a, b) => a+b, 0)).toLocaleString();
+    element.style.textAlign = "right";
+    element.style.textDecoration = "none";
+    element.style.cursor = "auto";
 }
 
 (function(open) {
@@ -88,12 +120,17 @@ async function calculateTotalItemValue(items, element)
                          amount: 1
                         }));
 
+                    /*data.push({id: "530", time: stringifyDate(new Date(Date.now())), name: "Can of Munster", amount: 1});
+                    data.push({id: "532", time: stringifyDate(new Date(Date.now())), name: "Can of Red Cow", amount: 1});
+                    data.push({id: "553", time: stringifyDate(new Date(Date.now())), name: "Can of Santa Shooters", amount: 1});
+                    data.push({id: "555", time: stringifyDate(new Date(Date.now())), name: "Can of X-MASS", amount: 12345});*/
+
                     data = Object.values(data.reduce(function(a, b)
                     {
                         if(Object.keys(a).includes(b.id))
                         {
-                            console.log(a[b.id].amount);
                             a[b.id].amount++;
+                            a[b.id].time = stringifyDate(new Date(parseInt(b.ts, 36)*1000));
                         }
                         else
                         {
@@ -103,17 +140,61 @@ async function calculateTotalItemValue(items, element)
                         return a;
                     }, {}));
 
-                    console.log(data);
-
                     var div = document.createElement("div");
-                    div.style.clear = "left";
-                    div.style.marginBottom = "-15px";
-                    div.innerHTML = "<div>Items on map: " + data.reduce((a, b) => a + b.amount, 0) + "</div>";
-                    div.innerHTML += "<div id='totalItemValue'>Total value: <span><span style='text-decoration: underline; cursor: pointer'>Calculate!</span></span></div>";
+
+                    var html = "";
+
+                    html += `
+<table id="cityFindTable">
+<thead>
+<tr>
+<th>Item</th>
+<th>Amount</th>
+<th>Value</th>
+</tr>
+</thead>
+<tbody>`;
+                    for(let item of data.sort((a, b) => a.time > b.time ? -1 : 1))
+                    {
+                        html += `
+<tr>
+<td>${item.name}</td>
+<td style="text-align: center">${item.amount}</td>
+<td id="cityFind-${item.id}" style="text-align: center">Unknown</td>
+</tr>
+`;
+                    }
+
+                    html += `
+</tbody>
+<tfoot>
+<tr>
+<th>Total:</th>
+<th id="cityFind-total">0</th>
+<th id="cityFind-sum" style="text-align: center; text-decoration: underline; cursor: pointer">Calculate!</th>
+</tr>
+</tfoot>
+</table>
+
+`;
+
+                    div.innerHTML += html;
 
                     document.querySelector("h4").after(div);
+                    document.querySelector("#cityFind-total").innerHTML = data.reduce((a, b) => a + b.amount, 0);
 
-                    document.querySelectorAll("#totalItemValue span")[1].addEventListener("click", function(data){return function(){calculateTotalItemValue(data, this)}}(data));
+                    resizeTable();
+
+                    document.querySelector("#cityFind-sum").addEventListener("click", function(data)
+                    {
+                        return function()
+                        {
+                            if(document.querySelector("#cityFind-sum").innerHTML == "Calculate!")
+                            {
+                                calculateTotalItemValue(data, this);
+                            }
+                        }
+                    }(data));
                 }
             });
         }
@@ -122,3 +203,63 @@ async function calculateTotalItemValue(items, element)
     };
 
 })(XMLHttpRequest.prototype.open);
+
+GM_addStyle(`
+
+#cityFindTable, #cityFindTable *
+{
+    all: revert;
+}
+
+#cityFindTable
+{
+    clear: left;
+    margin: 4px 0 -15px;
+    border-collapse: collapse;
+}
+
+#cityFindTable td, #cityFindTable th
+{
+    border-collapse: collapse;
+    border: 1px solid #000;
+}
+
+#cityFindTable td
+{
+    border-bottom: none;
+}
+
+#cityFindTable tr:first-child td
+{
+    border-top: none;
+}
+
+#cityFindTable thead, #cityFindTable tfoot, #cityFindTable tr
+{
+    display: table;
+}
+
+#cityFindTable th
+{
+    background: #eee;
+}
+
+#cityFindTable td
+{
+    background: #ddd;
+}
+
+#cityFindTable tbody
+{
+    display: block;
+    max-height: 100px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+}
+
+#cityFindTable th, #cityFindTable td
+{
+    padding: 5px;
+}
+
+`);
