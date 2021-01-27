02977609ac4b8bb921ab9fbe306fcf25004dca95
AquaRegia <emanuel.malmquist@gmail.com>
2021-01-27 21:21:42 +0100

    Create Torn AquaTools.user.js

---
+++ Torn/Torn AquaTools.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Torn AquaTools
+// @namespace    
+// @version      1.0
+// @description  
+// @author       AquaRegia
+// @match        https://www.torn.com/*
+// @updateURL    https://github.com/AquaRegia/Tampermonkey/raw/main/Torn/Torn%20AquaTools.js
+// @downloadURL  https://github.com/AquaRegia/Tampermonkey/raw/main/Torn/Torn%20AquaTools.js
+// @grant        GM_addStyle
+// @grant        GM_notification
+// ==/UserScript==
+
