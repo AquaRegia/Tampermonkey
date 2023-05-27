41d9211c8b3975f5fa6ff75f5cde7dd528bb9b0b
AquaRegia <emanuel.malmquist@gmail.com>
2023-05-27 16:00:12 +0200

    Update Grayscale Everything.user.js

--- Misc/Grayscale Everything.user.js
+++ Misc/Grayscale Everything.user.js
@@ -1,12 +1,42 @@
 // ==UserScript==
 // @name         Grayscale Everything
 // @namespace    
-// @version      0.1
+// @version      0.2
 // @description  Remove all colors from every website
 // @author       AquaRegia
 // @match        *://*/*
+// @grant        GM_registerMenuCommand
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        GM_addStyle
-// @run-at       document-start
+// @run-at       document_start
 // ==/UserScript==
 
-GM_addStyle("html{filter: grayscale(1);}");
+(function() {
+    'use strict';
+    let style = GM_addStyle("html{filter: grayscale(1) !important;}");
+
+    function toggle()
+    {
+        if(!style.disabled)
+        {
+            style.disabled = true;
+            GM_setValue("disabled", true);
+        }
+        else
+        {
+            style.disabled = false;
+            GM_setValue("disabled", false);
+        }
+    }
+
+    let disabled = GM_getValue("disabled", false);
+
+    if(disabled)
+    {
+        toggle();
+    }
+
+    GM_registerMenuCommand("Toggle", toggle);
+
+})();
